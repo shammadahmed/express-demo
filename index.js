0176f573dd7f66ad2f9a80d52cb4ace92310a798
Hammad Ahmed <shammad.c5@hotmail.com>
2019-01-11 11:30:49 +0500

    add url logging

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.set('view engine', 'hbs');
 
+app.use((req, res, next) => {
+    console.log(`${req.url} - ${new Date()}`);
+    next();
+});
+
 app.get('/', (req, res) => res.render('greeting', {name: 'World'}));
 
 app.get('/names/:name', (req, res) => res.render('greeting', {name: req.params.name}));
